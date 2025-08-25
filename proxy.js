c5125bf1b443831ede0fc2299d742d7b5d5d2e7e
abel <abeloabate01@gmail.com>
2025-08-25 16:27:01 +0300

    26

--- proxy.js
+++ proxy.js
@@ -29,9 +29,7 @@ app.use("/proxy", (req, res, next) => {
             return url.pathname + url.search;
         },
         proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-            // Use srcReq.query.url instead of targetUrl
-            const streamUrl = srcReq.query.url;
-            if (streamUrl && streamUrl.includes("edgenextcdn.net")) {
+            if (targetUrl.includes("edgenextcdn.net")) {
                 proxyReqOpts.headers['Referer'] = "https://www.shahid.net/";
                 proxyReqOpts.headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
                 proxyReqOpts.headers['Origin'] = "https://www.shahid.net";
